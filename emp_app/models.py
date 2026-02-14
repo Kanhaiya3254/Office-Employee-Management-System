8b0eace6bd82d7e76e844c3dd60c6eb530cb7d3e
Kanhaiya Jha <kanhaiyakumar3254@gmail.com>
2026-02-15 00:49:42 +0530

    Updated full file with latest changes

--- emp_app/models.py
+++ emp_app/models.py
@@ -1,28 +1,30 @@
 from django.db import models
 
+# Create your models here.
+
 class Department(models.Model):
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-
+    name = models.CharField(max_length=100,null=False)
+    location=models.CharField(max_length=100)
+    
     def __str__(self):
         return self.name
-
-
+    
 class Role(models.Model):
-    name = models.CharField(max_length=100)
-
+    name = models.CharField(max_length=100, null=False)
+    
     def __str__(self):
         return self.name
-
 
 class Employee(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    salary = models.IntegerField()
-    bonus = models.IntegerField()
-    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, null=False)
+    last_name = models.CharField(max_length=100,)
     dept = models.ForeignKey(Department, on_delete=models.CASCADE)
-    hire_date = models.DateTimeField(auto_now_add=True)
-
+    salary = models.IntegerField(default=0)
+    bonus = models.IntegerField(default=0)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    phone = models.IntegerField(default=0)
+    hire_date = models.DateField()
+    
+    
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return "%s %s %s" %(self.first_name, self.last_name, self.phone)
--- emp_app/views.py
+++ emp_app/views.py
@@ -1,52 +1,73 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, HttpResponse
 from .models import Employee, Role, Department
+from datetime import datetime
+from django.db.models import Q
 
+# Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def add_emp(request):
-    roles = Role.objects.all()
-    depts = Department.objects.all()
-    if request.method == 'POST':
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        salary = request.POST.get('salary')
-        bonus = request.POST.get('bonus')
-        role_id = request.POST.get('role')
-        dept_id = request.POST.get('dept')
-
-        emp = Employee(
-            first_name=first_name,
-            last_name=last_name,
-            salary=salary,
-            bonus=bonus,
-            role=Role.objects.get(id=role_id),
-            dept=Department.objects.get(id=dept_id)
-        )
-        emp.save()
-        return redirect('all_emp')
-    context = {'roles': roles, 'depts': depts}
-    return render(request, 'add_emp.html', context)
-
 def all_emp(request):
     emps = Employee.objects.all()
-    return render(request, 'all_emp.html', {'emps': emps})
+    context = {
+        'emps' : emps
+    }
+    print(context)
+    return render(request, 'view_all_emp.html', context)
 
-def remove_emp(request):
+
+def add_emp(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        salary = int(request.POST['salary'])
+        bonus = int(request.POST['bonus'])
+        phone = int(request.POST['phone'])
+        dept = int(request.POST['dept'])
+        role = int(request.POST['role'])
+        new_emp = Employee(first_name= first_name, last_name=last_name, salary=salary, bonus=bonus, phone=phone, dept_id=dept, role_id=role, hire_date=datetime.now())
+        new_emp.save()
+        return HttpResponse('Employee added Successfully')
+    elif request.method=='GET':
+        return render(request, 'add_emp.html')
+    else:
+        return HttpResponse("An Exception Occured! Employee Has Not Been Added")
+
+
+def remove_emp(request, emp_id = 0):
+    if emp_id:
+        try:
+            emp_to_be_removed = Employee.objects.get(id=emp_id)
+            emp_to_be_removed.delete()
+            return HttpResponse("Employee Removed Successfully")
+        except:
+            return HttpResponse("Please Enter A Valid EMP ID")
     emps = Employee.objects.all()
-    return render(request, 'remove_emp.html', {'emps': emps})
+    context = {
+        'emps': emps
+    }
+    return render(request, 'remove_emp.html', context)
 
 def filter_emp(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        dept = request.POST.get('dept')
-        role = request.POST.get('role')
+        name = request.POST['name']
+        dept = request.POST['dept']
+        role = request.POST['role']
         emps = Employee.objects.all()
         if name:
-            emps = emps.filter(first_name__icontains=name) | emps.filter(last_name__icontains=name)
+            emps = emps.filter(Q(first_name__icontains = name) | Q(last_name__icontains = name))
         if dept:
-            emps = emps.filter(dept__name__icontains=dept)
+            emps = emps.filter(dept__name__icontains = dept)
         if role:
-            emps = emps.filter(role__name__icontains=role)
-        return render(request, 'filter_emp.html', {'emps': emps, 'name': name, 'dept': dept, 'role': role})
-    return render(request, 'filter_emp.html')
+            emps = emps.filter(role__name__icontains = role)
+        
+        context = {
+            'emps' : emps
+        }
+        return render(request, 'view_all_emp.html', context)
+
+    elif request.method == 'GET':
+        return render(request, 'filter_emp.html')
+
+    else:
+        return HttpsResponse('An Exceptional Occurred')
